21ab153178c9b9fc97b2c08a1fa28c8902b65d1e
Evgeny Leonov <eugene.leonov@merck.com>
2023-11-02 20:38:18 +0100

    Update oregon_trail.clj

--- clojure-course/src/clojure_course/week6/oregon_trail.clj
+++ clojure-course/src/clojure_course/week6/oregon_trail.clj
@@ -1,4 +1,4 @@
-(ns clojure-course.week6.oregon-trail
+(ns clojure-course.week6.oregon_trail
   (:require [clojure-course.week6.graph_common_funcs :as funcs])
   )
 
@@ -22,15 +22,20 @@
    (Edge. 1 "A" "B" -30 7)
    (Edge. 2 "A" "C" -20 10)
    (Edge. 3 "B" "D" -20 4)
-   (Edge. 4 "C" "D" -10 5)
+   (Edge. 3 "A" "D" -20 4)
+   ; (Edge. 4 "C" "D" -10 5)
    ]
   )
 
 (defn print_movement_history
   [node_name
    movements_history]
-  (println "history for" node_name ":" (reverse @movements_history))
+  (println "history for" node_name ":" @movements_history)
+  ;  (reverse @movements_history)
   )
+
+(def all_routes (atom []))
+(def start_node_name "A")
 
 (defn dfs_graph
   [
@@ -39,8 +44,12 @@
    ]
   (let [edges (funcs/get_edges_for_node current_node all_edges)
         current_node_name (:name current_node)
+        local_history []
         ]
     (println current_node_name "is visited")
+    (swap! movements_history conj current_node_name)
+    (print_movement_history current_node_name movements_history)
+
     (doseq [edge edges
             :let [
                   child_node (funcs/get_another_node_for_edge current_node edge all_nodes)
@@ -49,15 +58,37 @@
             :when (not (.contains @movements_history child_node_name))
             ]
       (println "edge=" (:node1 edge) "<->" (:node2 edge)" Let's go from" current_node_name "to" child_node_name)
-      (swap! movements_history conj current_node_name)
-      (print_movement_history current_node_name movements_history)
       (dfs_graph child_node movements_history)
+      (println "recursion returns to" current_node_name)
+      (when (= current_node_name start_node_name)
+        ; save movement history to collection of routes
+        (swap! all_routes conj movements_history)
+        ; set the history empty for other routes
+        ;(reset! movements_history (atom []))
+        (println "Route saved" all_routes)
+        )
       )
+    movements_history
     )
   )
 
-(dfs_graph (funcs/get_node "A" all_nodes) (atom []))
+(defn find_all_routes
+  [start_node_name
+   end_node_name
+   ]
+  (let [start_node (funcs/get_node start_node_name all_nodes)]
 
+
+    )
+
+
+  )
+
+(dfs_graph (funcs/get_node start_node_name all_nodes) (atom []))
+(doseq [route @all_routes]
+  (println @route)
+
+  )
 
 
 
