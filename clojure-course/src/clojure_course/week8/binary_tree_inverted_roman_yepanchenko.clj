0c7d1b8a456eff79419ac78e3644ad88bad0a30c
Evgeny Leonov <145992308+EvgenyLeonov@users.noreply.github.com>
2023-11-14 21:18:30 +0100

    Update binary_tree_inverted_roman_yepanchenko.clj

--- clojure-course/src/clojure_course/week8/binary_tree_inverted_roman_yepanchenko.clj
+++ clojure-course/src/clojure_course/week8/binary_tree_inverted_roman_yepanchenko.clj
@@ -1,29 +1,7 @@
 (ns clojure-course.week8.binary-tree-inverted-roman-yepanchenko)
-
 (defrecord Node [name children])
 
-(def nodes (Node. 2 [(Node. 1 [(Node. 5 []) (Node. 9 [])]) (Node. 3 [])]))
-
-(defn process2
-  [node1, node2]
-
-  (if (< (:name node1) (:name node2))
-    (do
-      {
-       :root  (:name node2)
-       :child (:name node1)
-       }
-      )
-
-    (do
-      {
-       :root  (:name node1)
-       :child (:name node2)
-       }
-      )
-    )
-  )
-
+(def nodes (Node. 4 [(Node. 2 [(Node. 1 []) (Node. 3 [])]) (Node. 5 [(Node. 6 []) (Node. 7 [])])]))
 
 (defn process
   [node]
@@ -31,57 +9,31 @@
   (let
     [
      nodeTemp (atom nil)
-     result (atom {:root nil, :child nil})
-     childIndex (atom 0)
-     processResponse (atom nil)
-     hasChanges (atom false)
+     childTemp (atom nil)
+     childrenTemp (atom [])
      ]
 
     (reset! nodeTemp node)
 
-    (doseq [value (:children @nodeTemp)]
+    (if (= (count (:children @nodeTemp)) 2)
+      (do
 
-      (reset! processResponse (process value))
+        (reset! childTemp (get (:children @nodeTemp) 0))
 
-      (if (= (:has-changes @processResponse) true)
-        (reset! hasChanges true)
+        (reset! childrenTemp (assoc @childrenTemp 0 (process (get (:children @nodeTemp) 1))))
+        (reset! childrenTemp (assoc @childrenTemp 1 (process @childTemp)))
+
+        (reset! nodeTemp (assoc @nodeTemp :children @childrenTemp))
         )
-
-      (reset! nodeTemp
-              (assoc @nodeTemp :children (assoc (:children @nodeTemp) @childIndex (:node @processResponse)))
-              )
-
-
-      (reset! childIndex (inc @childIndex))
       )
 
-    (reset! childIndex 0)
-
-    (doseq [value (:children @nodeTemp)]
-
-      (reset! result (process2 @nodeTemp value))
-
-      (if (not (= (:root @result) (:name @nodeTemp)))
-        (reset! hasChanges true)
-        )
-
-      (reset! nodeTemp (assoc @nodeTemp :name (:root @result)))
-
-      (reset! nodeTemp
-              (assoc @nodeTemp :children (assoc (:children @nodeTemp) @childIndex (assoc value :name (:child @result))))
-              )
-
-      (reset! childIndex (inc @childIndex))
-      )
-
-    {:has-changes @hasChanges :node @nodeTemp}
+    @nodeTemp
     )
   )
 
-
-
 (defn print_binary_tree_routines
   [node]
+
   (when (some? node)
 
     (let [left_node (get (:children node) 0)
@@ -91,16 +43,14 @@
       (when (some? left_node)
         (print (:name left_node))
         )
+
       (when (some? right_node)
         (print (:name right_node))
-
         )
 
       (print_binary_tree_routines left_node)
       (print_binary_tree_routines right_node)
-
       )
-
     )
   )
 
@@ -110,36 +60,8 @@
   (print_binary_tree_routines root_node)
   )
 
-(print "old: " )
+(print "old: ")
 (print_binary_tree nodes)
 (println "")
-(let [
-      tempNode (atom nil)
-      state (atom true)
-      counter (atom 0)
-      processResponse (atom nil)
-      ]
-
-  (reset! tempNode nodes)
-
-  (while (= @state true)
-    (reset! processResponse (process @tempNode))
-
-
-    (reset! tempNode (:node @processResponse))
-
-    (reset! counter (inc @counter))
-
-    (if (= (:has-changes @processResponse) false)
-      (reset! state false)
-      )
-
-    (println "step" @counter)
-    )
-
-  (print "updated:" )
-  (print_binary_tree @tempNode)
-  )
-
-
-
+(print "updated ")
+(print_binary_tree (process nodes))
