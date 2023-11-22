d74b6bd344b7745c32c862a44dd3e23f6b71ecd5
Evgeny Leonov <145992308+EvgenyLeonov@users.noreply.github.com>
2023-11-22 21:54:12 +0100

    current progress

--- clojure-course/src/clojure_course/week9/loan_data_analysis.clj
+++ clojure-course/src/clojure_course/week9/loan_data_analysis.clj
@@ -1,4 +1,11 @@
-(ns clojure-course.week9.loan-data-analysis)
+(ns clojure-course.week9.loan-data-analysis
+  (:require [clojure.string :as string :only [split]])
+  )
+
+(defn read_dataset [filepath]
+  (with-open [rdr (clojure.java.io/reader filepath)]
+    (let [line (line-seq rdr)]
+      (clojure.string/join "\n" line))))
 
 (defn read_customer_personal_data []
   (rest
@@ -15,15 +22,52 @@
   )
 
 (defn read_payments []
-  (rest
-    (with-open [rdr (clojure.java.io/reader "src/clojure_course/week9/payments.txt")]
-      (reduce conj [] (line-seq rdr)))
-    )
+  (rest (with-open [rdr (clojure.java.io/reader "src/clojure_course/week9/payments.txt")]
+          (reduce conj [] (line-seq rdr))))
+  )
+
+(defn get_missed_months [prev_date current_date]
+  1
   )
 
 (defn get_missed_payments_by_customer
-  [customer_name]
+  [customer_name payments_data]
+  (let [current_loan (atom "")
+        prev_date (atom "")
+        missed_payments_count (atom 0)]
+    (for [payment_info payments_data]
+      (let [payment_info_tokens (clojure.string/split payment_info #",")
+            _ (println "payment_info=" payment_info)
+            person_name (first payment_info_tokens)
+            loan_name (second payment_info_tokens)
+            current_date (str (get payment_info_tokens 2))
+            ]
+        (println "customer_name=" customer_name)
+        (when (= customer_name person_name)
+          (if (not= @current_loan loan_name)
+            (do
+              (println "there")
+              (reset! current_loan loan_name)
+              (reset! prev_date current_date)
+              )
+            (do
+              (println "here")
+              (swap! missed_payments_count + (get_missed_months @prev_date current_date))
+              (reset! prev_date current_date)
+              )
+            )
+          )
+        )
+      )
+    @missed_payments_count
+    )
   )
+
+
+(println (get_missed_payments_by_customer "Charlie Holmes" (read_payments)))
+
+
+
 
 
 
