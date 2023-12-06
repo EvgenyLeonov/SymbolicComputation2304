ab3b7497970fc49c4e76adfbe384dabd2c8f48ba
Evgeny Leonov <leonov.e@praguecityuniversity.cz>
2023-12-06 10:31:44 +0100

    Update scene_rendering.clj

--- clojure-course/src/clojure_course/week11/scene_rendering.clj
+++ clojure-course/src/clojure_course/week11/scene_rendering.clj
@@ -1,7 +1,10 @@
 (ns clojure-course.week11.scene_rendering)
 
+(def SCENE_WIDTH 15)
+(def SCENE_HEIGHT 6)
+
 ; 13 x 6
-(def scene_content
+(def scene_content_demo
   [["|" "\\" " " " " " " " " " " " " " " " " " " " " "/" "|"]
    ["|" " " "|" "\\" " " "_" "_" "_" "_" " " "/" "|" " " "|"]
    ["|" " " "|" " " "|", " " " " " " " ", "|" "" " " "|" " " "|"]
@@ -27,5 +30,70 @@
 
   )
 
-(render_scene scene_content)
+(render_scene scene_content_demo)
+
+; all are boolean
+(defrecord Scene_definition [c1 c2 c3 l1 l2 r1 r2])
+
+(def scene1 (Scene_definition. false false true true true true true))
+(def scene2 (Scene_definition. false true false true true true true))
+
+(defn c1 [])
+(defn c2 [])
+(defn c3 [])
+(defn l1 []
+  (into []
+    (concat
+      (into [] (repeat SCENE_HEIGHT "|"))
+      ["\\" " " " " " " " " "/"]
+      )
+    )
+  )
+(defn l2 [])
+(defn r1 [])
+(defn r2 [])
+
+(def rendering_rules
+  ; render from left to right
+  {"C1" [c1]
+   "C2" [l1 c2 r1]
+   "C3" [l1 l2 c3 r2 r1]
+   }
+  )
+
+(defn prepare_rendering_instructions
+  [scene_definition]
+  (let [c3_set (:c3 scene_definition)
+        c2_set (:c2 scene_definition)
+        c1_set (:c2 scene_definition)
+        rule_key (cond
+                   (true? c3_set) "C3"
+                   (true? c2_set) "C2"
+                   (true? c1_set) "C1"
+                   )
+        rule (get rendering_rules rule_key)
+        ;_ (println "rules=" rules)
+        ]
+    rule
+    )
+  )
+
+;(prepare_rendering_instructions scene1)
+;(prepare_rendering_instructions scene2)
+
+(defn render_instructions_to_scene
+  [rule scene_definition]
+  (let [scene (atom [])]
+    (loop [index 0]
+      (when (< index (count rule))
+        (let [rule (get rule index)]
+          (swap! scene conj (rule))
+          )
+        (recur (inc index))
+        )
+      )
+
+    )
+
+  )
 
