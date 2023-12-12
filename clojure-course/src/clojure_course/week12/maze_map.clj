d2e4651ed09434233d94ecae208abf5f9b713154
Evgeny Leonov (MSD) <leonov.e@praguecityuniversity.cz>
2023-12-12 14:01:30 +0100

    temp checkin

--- clojure-course/src/clojure_course/week12/maze_map.clj
+++ clojure-course/src/clojure_course/week12/maze_map.clj
@@ -20,18 +20,6 @@
    ]
   )
 
-; l1 l2 c1 c2 c3 r1 r2
-(def sight_modifiers {
-                      "S" [
-                           [1 0]
-                           [1 1]
-                           [0 1]
-                           [0 2]
-                           [0 3]
-                           [-1 1]
-                           ]
-                      })
-
 (defn render_maze_map [maze_map direction_of_sight]
   (loop [row 0]
     (when (< row (count maze_map))
@@ -50,11 +38,11 @@
                                   (= "E" direction_of_sight_upper) ">"
                                   )
                   item_to_render (cond
-                                   (= item 0) "  "
-                                   (= item 1) (str player_symbol " ")
-                                   (= item 2) "[]"
-                                   (= item 3) "A "
-                                   (= item 4) "F "
+                                   (= item 0) " "
+                                   (= item 1) player_symbol
+                                   (= item 2) "#"
+                                   (= item 3) "A"
+                                   (= item 4) "F"
                                    )
                   ]
               (if (= last_column column)
@@ -69,18 +57,24 @@
     )
   )
 
-(render_maze_map maze_map "N")
-(render_maze_map maze_map "S")
-(render_maze_map maze_map "W")
-(render_maze_map maze_map "E")
-
 (defn get_maze_item
-  [row column]
-  ; TODO
+  [maze row column]
+  (let [last_row_index (dec (count maze))
+        ; assumption is the maze has at least one row
+        last_column_index (dec (count (get maze 0)))]
+    (if (and (>= row) (<= row last_row_index)
+             (>= column) (<= column last_column_index)
+             )
+      (let [row_content (get maze row)]
+        (int (get row_content column))
+        )
+      nil
+      )
+    )
   )
 
 (defn can_move?
-  [current_row current_column direction_of_sight maze]
+  [maze current_row current_column direction_of_sight]
   (let [
         direction_of_sight_upper (clojure.string/upper-case direction_of_sight)
         modificator_row (cond
@@ -88,17 +82,90 @@
                           (= "S" direction_of_sight_upper) 1
                           )
         modificator_column (cond
-                          (= "W" direction_of_sight_upper) -1
-                          (= "E" direction_of_sight_upper) 1
-                          )
+                             (= "W" direction_of_sight_upper) -1
+                             (= "E" direction_of_sight_upper) 1
+                             )
         new_row (+ current_row modificator_row)
         new_column (+ current_column modificator_column)
         last_row_index (dec (count maze))
         ;:when (>= last_row_index 0)
         last_column_index (dec (count (get maze 0)))
-        result (not= WALL (get_maze_item new_row new_column))
+        result (not= WALL (get_maze_item maze new_row new_column))
         ]
     result
     )
   )
 
+(defrecord Scene_definition [c1 c2 c3 l1 l2 r1 r2])
+
+(defn is_wall?
+  [maze row column]
+  (let [v (get_maze_item maze row column)]
+    ;(println "v =" v)
+    (if (nil? v)
+      true
+      (= WALL v)
+      )
+    )
+  )
+(defn is_wall?_vec
+  [maze row_col_vec]
+  (is_wall? maze (int (first row_col_vec)) (int (second row_col_vec)))
+  )
+
+(defn apply_sight_modifiers
+  [current_row current_column sight_modifiers index]
+  (let [modif_vec (get sight_modifiers index)
+        ;_ (println "modif_vec =" modif_vec)
+        ;_ (println "sight_modifiers =" sight_modifiers)
+        ;_ (println "index =" index)
+        modif_pos [(+ current_row (first modif_vec)) (+ current_column (second modif_vec))]
+        ]
+    modif_pos
+    )
+  )
+
+(defn get_scene_definition
+  [maze current_row current_column direction_of_sight]
+  (let [direction_of_sight_upper (clojure.string/upper-case direction_of_sight)
+        ;_ (println "direction_of_sight_upper =" direction_of_sight_upper)
+        ; c1 c2 c3 l1 l2 r1 r2
+        sight_modifiers_definition {
+                                    "S" [[1 0]
+                                         [2 0]
+                                         [3 0]
+                                         [1 1]
+                                         [2 1]
+                                         [1 -1]
+                                         [2 -1]
+                                         ]
+                                    }
+        sight_modifiers (get sight_modifiers_definition direction_of_sight_upper)
+        ]
+    (Scene_definition.
+      (is_wall?_vec maze (apply_sight_modifiers current_row current_column sight_modifiers 0))
+      (is_wall?_vec maze (apply_sight_modifiers current_row current_column sight_modifiers 1))
+      (is_wall?_vec maze (apply_sight_modifiers current_row current_column sight_modifiers 2))
+      (is_wall?_vec maze (apply_sight_modifiers current_row current_column sight_modifiers 3))
+      (is_wall?_vec maze (apply_sight_modifiers current_row current_column sight_modifiers 4))
+      (is_wall?_vec maze (apply_sight_modifiers current_row current_column sight_modifiers 5))
+      (is_wall?_vec maze (apply_sight_modifiers current_row current_column sight_modifiers 6))
+      )
+    )
+  )
+
+; ======= DEBUG =========
+
+;(render_maze_map maze_map "N")
+;(render_maze_map maze_map "S")
+;(render_maze_map maze_map "W")
+;(render_maze_map maze_map "E")
+
+;(println (is_wall? maze_map 1 1))
+;(println (is_wall? maze_map 3 1))
+;(println (is_wall? maze_map 1 3))
+
+;(println (get_scene_definition maze_map 1 1 "s"))
+
+
+; =======================
--- clojure-course/src/clojure_course/week12/pacman_fps.clj
+++ clojure-course/src/clojure_course/week12/pacman_fps.clj
@@ -3,18 +3,25 @@
   (:require [clojure-course.week12.maze_map :as maze_map_rendering])
   )
 
+(def maze_map maze_map_rendering/maze_map)
+
 (def direction_of_sight (atom "S"))
-(def coord_row 2)
-(def coord_column 1)
+(def pacman_current_row (atom 1))
+(def pacman_current_column (atom 1))
 
 (defn main_loop
   []
-  (let [scene_instr (scene_rendering/prepare_instructions_for_scene scene_rendering/scene_c3)]
+  (let [
+        ;scene_instr (scene_rendering/prepare_instructions_for_scene scene_rendering/scene_c3)
+        scene_def (maze_map_rendering/get_scene_definition maze_map @pacman_current_row @pacman_current_column @direction_of_sight)
+        _ (println "scene_def =" scene_def)
+        scene_instr (scene_rendering/prepare_instructions_for_scene scene_def)
+        ]
     ; hack to clear the REPL screen
-    (print (str (char 27) "[2J"))
+    ;(print (str (char 27) "[2J"))
     (scene_rendering/render_scene scene_instr)
     (println)
-    (maze_map_rendering/render_maze_map maze_map_rendering/maze_map direction_of_sight)
+    (maze_map_rendering/render_maze_map maze_map @direction_of_sight)
     )
   )
 
