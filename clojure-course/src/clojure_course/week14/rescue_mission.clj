d8f014765939c86b2e06eca39560e3337c3826de
Evgeny Leonov (MSD) <30621852+Garmoska@users.noreply.github.com>
2024-01-15 17:09:52 +0100

    rescue mission

--- clojure-course/src/clojure_course/week14/rescue_mission.clj
+++ clojure-course/src/clojure_course/week14/rescue_mission.clj
@@ -2,12 +2,13 @@
   (:require [clojure-course.week14.rescue_mission_data :as data])
   )
 
-(def airplane_x 60)
-(def airplane_y_min 30)
-(def airplane_y_max 60)
-(def max_distance_of_transmission 2)
+(def airplane_x 50)
+(def airplane_y 57)
+(def max_distance_of_transmission 10)
 (def airplane_x_min (- airplane_x max_distance_of_transmission))
 (def airplane_x_max (+ airplane_x max_distance_of_transmission))
+(def airplane_y_min (- airplane_y max_distance_of_transmission))
+(def airplane_y_max (+ airplane_y max_distance_of_transmission))
 (def suitable_points (atom []))
 
 (defn linear_function [point1 point2]
@@ -17,7 +18,7 @@
         y1 (int (last point1))
         x2 (int (first point2))
         y2 (int (last point2))
-        _ (println "x1=" x1 "; x2=" x2 "; y1=" y1 "; y2=" y2)
+        ;_ (println "x1=" x1 "; x2=" x2 "; y1=" y1 "; y2=" y2)
         k (cond
             (= x1 x2) 0
             :else (/ (- y2 y1) (- x2 x1))
@@ -52,29 +53,27 @@
                  :else xi
                  )
         x_to (cond
-                 (> xi xa) xi
-                 :else xa
-                 )
+               (> xi xa) xi
+               :else xa
+               )
         lin_f (linear_function [xa ya] [xi yi])
         k (first lin_f)
         b (second lin_f)
         heights (atom [])
-        x_last (atom 0)
-        y_last (atom 0)
         result (atom [])
         ]
-    (println "x_from=" x_from "; x_to=" x_to)
+    ;(println "x_from=" x_from "; x_to=" x_to)
     (loop [x x_from]
       (when (<= x x_to)
         (let [y (int (Math/ceil (+ (* k x) b)))
               z (get_height x y)
-              _ (println "x=" x "; y=" y "; z=" z "; k=" k "; b=" b)
+              ;_ (println "x=" x "; y=" y "; z=" z "; k=" k "; b=" b)
               ]
           (when (> z 0)
             (swap! heights conj z)
-            (reset! x_last x)
-            (reset! y_last y)
-            (println "xx x_last=" @x_last "; y_last=" @y_last)
+            ;(reset! x_last x)
+            ;(reset! y_last y)
+            ;(println "xx x_last=" @x_last "; y_last=" @y_last)
             )
           )
         (recur (inc x))
@@ -89,8 +88,9 @@
             valid? (= max_ind 0)
             ]
         (when (true? valid?)
-          (println "x_last=" @x_last "; y_last=" @y_last)
-          (reset! result (vec [@x_last @y_last]))
+          ;(println "x_last=" @x_last "; y_last=" @y_last)
+          ;(reset! result (vec [@x_last @y_last]))
+          (reset! result (vec [xi yi]))
           )
         )
       )
@@ -99,30 +99,96 @@
     )
   )
 
-(loop [x airplane_x_min]
-  (when (<= x airplane_x_max)
-    (loop [y airplane_y_min]
-      (when (<= y airplane_y_max)
-        (let [airplane_position (vec [airplane_x y])
-              ; we scan horizontally only
-              point_of_interest (vec [x y])
-              ;_ (println "point_of_interest=" point_of_interest)
-              result (is_point_suits? airplane_position point_of_interest)]
-          (when (> (count result) 0)
-            (swap! suitable_points conj result)
+(defn find_radio_signal []
+  (loop [x airplane_x_min]
+    (when (<= x airplane_x_max)
+      (loop [y airplane_y_min]
+        (when (<= y airplane_y_max)
+          (let [airplane_position (vec [airplane_x airplane_y])
+                point_of_interest (vec [x y])
+                ;_ (println "point_of_interest=" point_of_interest)
+                result (is_point_suits? airplane_position point_of_interest)]
+            (when (> (count result) 0)
+              (swap! suitable_points conj result)
+              )
             )
+          (recur (inc y))
           )
-        (recur (inc y))
         )
+      (recur (inc x))
       )
-    (recur (inc x))
+    )
+  (doseq [p @suitable_points]
+    (println (first p) "," (last p))
     )
   )
 
-(doseq [p @suitable_points]
-  (println (first p) "," (last p))
+(defn find_potential_lzs []
+  ; works as stack
+  (let [
+        ; landing zones
+        lzs (atom [])
+        point_of_interest (atom nil)
+        ]
+    (loop [index 1]
+      (when (< index (count data/data))
+        (let [v1 (get data/data index)
+              v0 (get data/data (dec index))
+              x_1 (int (first v1))
+              y_1 (int (second v1))
+              z_1 (int (last v1))
+              x_0 (int (first v0))
+              y_0 (int (second v0))
+              z_0 (int (last v0))
+              y_poi (cond
+                      (nil? @point_of_interest) -1
+                      :else (int (second @point_of_interest))
+                      )
+              ]
+          (if (nil? @point_of_interest)
+            (when (and
+                    (= x_0 x_1)
+                    (= y_0 (dec y_1))
+                    (= (dec z_0) z_1)
+                    )
+              (reset! point_of_interest (vec [x_0 y_0 z_0]))
+              ;(println "point_of_interest set=" @point_of_interest)
+              )
+            (do
+              ;(println "z_0=" z_0 "; z_1=" z_1 "; y_point=" y_poi "; y_1=" y_1)
+              (cond
+                (and
+                  (= z_1 (int (last @point_of_interest)))
+                  ; min length of a landing zone is 3
+                  (>= (- y_1 y_poi) 3)
+                  )
+                (do
+                  (swap! lzs conj @point_of_interest)
+                  (reset! point_of_interest nil)
+                  )
+                ;;;;
+                (< z_0 z_1) (reset! point_of_interest nil)
+                )
+              )
+
+            )
+          (recur (inc index))
+          )
+        )
+      )
+    (println "points of interest:")
+    (doseq [p @lzs]
+      (println (first p) "," (second p))
+      )
+    )
   )
-;(println @suitable_points)
+
+
+(find_radio_signal )
+
+;(find_potential_lzs)
+
+
 
 ; DEBUG
 ;(def point1 [0 0])
--- dataset-generators/taiga/main.py
+++ dataset-generators/taiga/main.py
@@ -6,86 +6,118 @@ MAP_WIDTH = 100
 MAP_HEIGHT = 80
 
 main_summits = [
-    [20, 60, 30]
-    , [20, 25, 25]
-    , [70, 60, 15]
-    , [70, 25, 15]
-    , [90, 40, 25]
-    , [90, 10, 10]
-    , [90, 70, 10]
+	[20, 60, 30]
+	, [20, 25, 25]
+	, [70, 40, 20]
+	, [70, 70, 20]
+	, [70, 20, 20]
+	, [90, 40, 10]
+	, [90, 60, 20]
+	# , [90, 40, 25]
+	# , [90, 10, 10]
+	# , [90, 70, 10]
 ]
 
 main_map = np.zeros((MAP_WIDTH, MAP_HEIGHT))
 
 
 def generate_landscape():
-    for summit in main_summits:
-        add_mountain(summit)
-    for x in range(0, MAP_WIDTH):
-        for y in range(0, MAP_HEIGHT):
-            current_value = main_map[x][y]
-            if current_value == 0:
-                main_map[x][y] = random.randint(1, 2)
-    generate_js()
-    generate_clojure()
+	for summit in main_summits:
+		add_mountain(summit)
+	# uneven_surface()
+	add_landings()
+	generate_js()
+	generate_clojure()
+
+
+def uneven_surface():
+	for x in range(0, MAP_WIDTH):
+		for y in range(0, MAP_HEIGHT):
+			current_value = main_map[x][y]
+			if current_value == 0:
+				main_map[x][y] = random.randint(1, 2)
 
 
 def calc_height(summit, step):
-    x_summit = summit[0]
-    y_summit = summit[1]
-    z_summit = summit[2]
-    x_left = x_summit - step
-    x_right = x_summit + step
-    y_top = y_summit - step
-    y_bottom = y_summit + step
-    changed_points = []
-    for x in range(x_left, x_right + 1):
-        for y in range(y_top, y_bottom + 1):
-            if (0 <= x < MAP_WIDTH) and (0 <= y < MAP_HEIGHT):
-                current_value = main_map[x][y]
-                z = z_summit - step
-                if current_value < z:
-                    changed_points.append([x, y, z])
-    return changed_points
+	x_summit = summit[0]
+	y_summit = summit[1]
+	z_summit = summit[2]
+	x_left = x_summit - step
+	x_right = x_summit + step
+	y_top = y_summit - step
+	y_bottom = y_summit + step
+	changed_points = []
+	for x in range(x_left, x_right + 1):
+		for y in range(y_top, y_bottom + 1):
+			if (0 <= x < MAP_WIDTH) and (0 <= y < MAP_HEIGHT):
+				current_value = main_map[x][y]
+				z = z_summit - step
+				if current_value < z:
+					changed_points.append([x, y, z])
+	return changed_points
 
 
 def add_mountain(summit):
-    step = 1
+	step = 1
 
-    z_summit = summit[2]
-    main_map[summit[0]][summit[1]] = z_summit
+	z_summit = summit[2]
+	main_map[summit[0]][summit[1]] = z_summit
 
-    while z_summit - step > 0:
-        points_to_change = calc_height(summit, step)
-        if len(points_to_change) > 0:
-            for p in points_to_change:
-                x = p[0]
-                y = p[1]
-                main_map[x][y] = p[2]
-        step = step + 1
+	while z_summit - step > 0:
+		points_to_change = calc_height(summit, step)
+		if len(points_to_change) > 0:
+			for p in points_to_change:
+				x = p[0]
+				y = p[1]
+				main_map[x][y] = p[2]
+		step = step + 1
+
+
+def add_landings():
+	terraces = [
+		[60, 48]
+		, [39, 61]
+		, [25, 24]
+		, [61, 40]
+		, [63, 40]
+		, [60, 60]
+		, [60, 33]
+		, [61, 73]
+	]
+	lt = 4
+	for terrace in terraces:
+		x = terrace[0]
+		y = terrace[1]
+		z_start = main_map[x][y]
+		z = z_start - 1
+		for i in range(lt):
+			if i == lt - 1:
+				main_map[x][y + i] = z_start
+			else:
+				main_map[x][y + i] = z
 
 
 def generate_js():
-    output = "var data = new vis.DataSet();\n"
-    for x in range(0, MAP_WIDTH):
-        for y in range(0, MAP_HEIGHT):
-            val = int(main_map[x][y])
-            output = output + f"data.add([{{x:{x},y:{y},z:{val},t:0,style:{val}}}]);\n"
-    with open("data.js", "w") as f:
-        f.write(output)
+	output = "var data = new vis.DataSet();\n"
+	for x in range(0, MAP_WIDTH):
+		for y in range(0, MAP_HEIGHT):
+			val = int(main_map[x][y])
+			output = output + f"data.add([{{x:{x},y:{y},z:{val},t:0,style:{val}}}]);\n"
+	with open("data.js", "w") as f:
+		f.write(output)
 
 
 def generate_clojure():
-    output = "(ns clojure-course.week14.rescue_mission_data)\n"
-    output = output + "(def data [\n"
-    for x in range(0, MAP_WIDTH):
-        for y in range(0, MAP_HEIGHT):
-            val = int(main_map[x][y])
-            output = output + f"[{x} {y} {val}]\n"
-    output = output + "])\n"
-    with open("rescue_mission_data.clj", "w") as f:
-        f.write(output)
+	output = "(ns clojure-course.week14.rescue_mission_data)\n"
+	output = output + "(def data [\n"
+	for x in range(0, MAP_WIDTH):
+		for y in range(0, MAP_HEIGHT):
+			val = int(main_map[x][y])
+			output = output + f"[{x} {y} {val}]\n"
+	output = output + "])\n"
+	with open("rescue_mission_data.clj", "w") as f:
+		f.write(output)
 
 
 if __name__ == '__main__':
-    generate_landscape()
+	generate_landscape()
